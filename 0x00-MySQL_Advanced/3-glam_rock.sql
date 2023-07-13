c1a8facac2250af541117734eb6ff61e19e318e1
jaredatandi <jaredatandi07@gmail.com>
2023-07-13 06:05:22 +0300

    Create a filter

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- list a band by a style 
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
+
