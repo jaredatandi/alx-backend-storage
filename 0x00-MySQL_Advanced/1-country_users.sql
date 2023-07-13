344ac16f407ef14da87bda67d42e2def17901544
jaredatandi <jaredatandi07@gmail.com>
2023-07-13 04:54:50 +0300

    Use enum

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,6 @@
+CREATE table if not exists users (
+id integer primary key auto_increment,
+email varchar(255) not null unuique,
+name varchar(255),
+country enum('US', 'CO', 'TN') not null default 'US'
+);
