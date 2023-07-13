3815a21b7f1d2f1d9074a8e44bb5efc1cfad0ec0
jaredatandi <jaredatandi07@gmail.com>
2023-07-13 04:45:45 +0300

    Add an underscore

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- A script to create a table users
 CREATE table if not exists users (
-id integer primary key autoincrement,
+id integer primary key auto_increment,
 email varchar(255) not null unique,
 name varchar(255)
 );
