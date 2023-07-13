73ee4770481076636e6a2e2de9b6f1dd0e7b7835
jaredatandi <jaredatandi07@gmail.com>
2023-07-13 05:01:07 +0300

    Add documentation

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,5 +3,5 @@ CREATE table if not exists users (
 id integer primary key auto_increment,
 email varchar(255) not null unuique,
 name varchar(255),
-country ENUM('US', 'CO', 'TN') not null default 'US'
+country enum('US', 'CO', 'TN') not null default 'US'
 );
