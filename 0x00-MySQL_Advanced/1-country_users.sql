b2831144d2a259978362e90f8cc93e47d231b10c
jaredatandi <jaredatandi07@gmail.com>
2023-07-13 05:07:11 +0300

    Use primary key

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
--- a script to create a table users
+--  creates a table users
 CREATE table if not exists users (
-id integer primary key auto_increment,
+id INT NOT NULL AUTO_INCREMENT,
 email varchar(255) not null unuique,
 name varchar(255),
-country enum('US', 'CO', 'TN') not null default 'US'
+country ENUM('US', 'CO', 'TN') not null default 'US',
+PRIMARY KEY (id)
 );
