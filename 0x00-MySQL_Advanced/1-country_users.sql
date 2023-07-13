4796c32a43123cbcda24a411d76da759793f9497
jaredatandi <jaredatandi07@gmail.com>
2023-07-13 04:59:06 +0300

    Use uppercase for keyword

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,5 +2,5 @@ CREATE table if not exists users (
 id integer primary key auto_increment,
 email varchar(255) not null unuique,
 name varchar(255),
-country enum('US', 'CO', 'TN') not null default 'US'
+country ENUM('US', 'CO', 'TN') not null default 'US'
 );
