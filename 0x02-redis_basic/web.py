7baf2c77d948e982f94ddad2e795448c4a5f55a6
jaredatandi <jaredatandi07@gmail.com>
2023-07-19 22:34:45 +0300

    Cache results

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,18 @@
+import redis
+import requests
+
+
+def track_url_count(url: str, redis_client: redis.Redis) -> None:
+    count_key = f"count:{url}"
+    redis_client.incr(count_key)
+    redis_client.expire(count_key, 10)
+
+
+def get_page(url: str) -> str:
+    redis_client = redis.Redis()
+    response = requests.get(url)
+    content = response.text
+
+    track_url_count(url, redis_client)
+
+    return content
