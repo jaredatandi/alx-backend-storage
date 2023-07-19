6d2e1267e699662c188856c5c734d69c00f28fb5
jaredatandi <jaredatandi07@gmail.com>
2023-07-19 22:31:55 +0300

    Replay

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,7 @@ import uuid
 from functools import wraps
 from typing import Union, Callable
 
+
 def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
@@ -13,6 +14,7 @@ def count_calls(method: Callable) -> Callable:
         self._redis.incr(key)
         return method(self, *args, **kwargs)
     return wrapper
+
 
 def call_history(method: Callable) -> Callable:
     @wraps(method)
@@ -27,17 +29,19 @@ def call_history(method: Callable) -> Callable:
         return output
     return wrapper
 
+
 class Cache:
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+    def get(self, key: str, fn: Callable = None)\
+            -> Union[str, bytes, int, float]:
         if self._redis.exists(key):
             data = self._redis.get(key)
             if fn is not None:
@@ -50,5 +54,16 @@ class Cache:
 
     def get_int(self, key: str) -> int:
         return self.get(key, fn=int)
-    
-    
+
+    def replay(self, method_name: str) -> List[str]:
+        input_key = method_name + ":inputs"
+        output_key = method_name + ":outputs"
+
+        inputs = self._redis.lrange(input_key, 0, -1)
+        outputs = self._redis.lrange(output_key, 0, -1)
+
+        replay_history = []
+        for i in range(len(inputs)):
+            replay_history.append(f"Input: {inputs[i]}, Output: {outputs[i]}")
+
+        return replay_history
