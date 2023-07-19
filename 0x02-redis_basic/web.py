561e88231f1f483ac8d9116b342d65cd3ed17e47
jaredatandi <jaredatandi07@gmail.com>
2023-07-19 22:37:47 +0300

    Update documentation

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,12 +10,26 @@ import requests
 
 
 def track_url_count(url: str, redis_client: redis.Redis) -> None:
+    """_summary_
+
+    Args:
+        url (str): _description_
+        redis_client (redis.Redis): _description_
+    """
     count_key = f"count:{url}"
     redis_client.incr(count_key)
     redis_client.expire(count_key, 10)
 
 
 def get_page(url: str) -> str:
+    """_summary_
+
+    Args:
+        url (str): _description_
+
+    Returns:
+        str: _description_
+    """
     redis_client = redis.Redis()
     response = requests.get(url)
     content = response.text
